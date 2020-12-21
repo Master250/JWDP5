a62af45e82554060678553336e805bebe2f7ceb1
TIAMIYU <66704012+Master250@users.noreply.github.com>
2020-12-21 05:00:53 +0100

    ..

--- js/confirmation.js
+++ js/confirmation.js
@@ -1,5 +1,4 @@
 /************************************** Confirmation de la commade **************************************/
-/////////////////////////////////////// Confirmation de la commade /////////////////////////////////
 function messageConfirm(){
   const idOrderValid = localStorage.getItem("orderValid");
   const confirmation = document.getElementById("confirmation");
@@ -12,52 +11,46 @@ function messageConfirm(){
   confirmation.appendChild(smsConfirm);
   confirmation.appendChild(confirmationPrice);
 
-
-
-
-    let t1 = document.createElement("table");
-    let caption = document.createElement("caption")
-    caption.textContent = "Récapitulatif de la commande";
-    let lign = document.createElement("tr");
-    t1.appendChild(caption);
+  let t1 = document.createElement("table");
+  let caption = document.createElement("caption")
+  caption.textContent = "Récapitulatif de la commande";
+  let lign = document.createElement("tr");
+  t1.appendChild(caption);
     
-    let containPhoto = document.createElement("th");
-    let colonNom = document.createElement("th");
-    let colonPrix = document.createElement("th");
+  let containPhoto = document.createElement("th");
+  let colonNom = document.createElement("th");
+  let colonPrix = document.createElement("th");
 
-    let lignTotal = document.createElement("tr");
-    let lignPrixTotal = document.createElement("th");
-    let prixConfirme = document.createElement("td");
+  let lignTotal = document.createElement("tr");
+  let lignPrixTotal = document.createElement("th");
+  let prixConfirme = document.createElement("td");
 
-    colonPrix.textContent = "Prix";
-    colonNom.textContent = "Nom Article";
-    containPhoto.textContent = "Image Article";
+  colonPrix.textContent = "Prix";
+  colonNom.textContent = "Nom Article";
+  containPhoto.textContent = "Image Article";
     
-    
-    let tableau = document.getElementById("review");
+  let tableau = document.getElementById("review");
 
-    tableau.appendChild(t1);
-    t1.appendChild(lign);
-    lign.appendChild(containPhoto);
-    lign.appendChild(colonNom);
-    lign.appendChild(colonPrix);
-
-    
-
-    let i = 0;
-    const basketRecap = JSON.parse(localStorage.getItem("recap"));
-    basketRecap.products.forEach((element) => {
-    let lignProduct = document.createElement("tr");
-    lignProduct.setAttribute("id", element._id);
-    let photoProduct = document.createElement("img");
-    photoProduct.setAttribute("src", element.imageUrl);
-    photoProduct.setAttribute("width", "110px");
-    let nomProduct = document.createElement("td");
-    nomProduct.textContent = element.name;
-    nomProduct.setAttribute("width", "100px");
-    let prixProduct = document.createElement("td");
-    prixProduct.setAttribute("width", "100px");
-    prixProduct.textContent = element.price/100 +"€";
+  tableau.appendChild(t1);
+  t1.appendChild(lign);
+  lign.appendChild(containPhoto);
+  lign.appendChild(colonNom);
+  lign.appendChild(colonPrix);
+  
+  let i = 0;
+  const basketRecap = JSON.parse(localStorage.getItem("recap"));
+  basketRecap.products.forEach((element) => {
+  let lignProduct = document.createElement("tr");
+  lignProduct.setAttribute("id", element._id);
+  let photoProduct = document.createElement("img");
+  photoProduct.setAttribute("src", element.imageUrl);
+  photoProduct.setAttribute("width", "110px");
+  let nomProduct = document.createElement("td");
+  nomProduct.textContent = element.name;
+  nomProduct.setAttribute("width", "100px");
+  let prixProduct = document.createElement("td");
+  prixProduct.setAttribute("width", "100px");
+  prixProduct.textContent = element.price/100 +"€";
 
       
       
